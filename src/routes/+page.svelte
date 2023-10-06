bbbb552ac4180e9eb0a2a5eb2639ec95d841052f
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-10-06 23:25:07 +0300

    Updated home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,10 @@
   export let data;
   const sbApi = data.sbApi;
   const user = getUserDetails(sbApi);
+  let infoDiv;
+  function scrollToInfo() {
+    infoDiv.scrollIntoView();
+  }
 </script>
 
 <main>
@@ -19,24 +23,38 @@
         <h2>What would you like to do today?</h2>
         <div class="row my-3">
           <div class="col-sm mb-2">
-            <a
-              href={hrefs.newFlight.home.link}
-              class="btn btn-primary btn-lg w-100">Log New Flight</a
-            >
+            {#if user}
+              <a
+                href={hrefs.newFlight.home.link}
+                class="btn btn-primary btn-lg w-100">Log New Flight</a
+              >
+            {:else}
+              <a
+                href={hrefs.signup.home.link}
+                class="btn btn-primary btn-lg w-100">Join AeroLogger</a
+              >
+            {/if}
           </div>
-          <div class="col-sm">
-            <a
-              href={hrefs.logbook.home.link}
-              class="btn btn-outline-primary btn-lg w-100"
-            >
-              View Logbook</a
-            >
+          <div class="col-sm mb-2">
+            {#if user}
+              <a
+                href={hrefs.logbook.home.link}
+                class="btn btn-outline-primary btn-lg w-100"
+              >
+                View Logbook</a
+              >
+            {:else}
+              <button
+                class="btn btn-outline-primary btn-lg w-100"
+                on:click={scrollToInfo}>Explore AeroLogger</button
+              >
+            {/if}
           </div>
         </div>
       </div>
     {/await}
   </div>
-  <div class="text-bg-dark pb-5">
+  <div class="text-bg-dark pb-5" bind:this={infoDiv}>
     <div class="container font-google-gabarito">
       <div class="d-flex justify-content-center">
         <img src={logo} alt="Logo" class="img-fluid" />
@@ -75,6 +93,76 @@
             <a href={hrefs.contact.home.link} class="text-light">contact form</a
             >.
           </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-6 col-xl-4 mb-5">
+          <div class="card h-100">
+            <div class="card-header"><h1>Easy to Use</h1></div>
+            <div class="card-body fs-5">
+              <ul>
+                <li>
+                  To log a flight, simply go to the <a
+                    href={hrefs.newFlight.home.link}
+                    class="text-reset">new flight</a
+                  > page, and fill out the simple form, and your logbook will automatically
+                  get the most information about your time in the air.
+                </li>
+                <li>
+                  Your logbook automatically counts your hours for you, and
+                  display your data in a user-friendly way.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 col-xl-4 mb-5">
+          <div class="card h-100">
+            <div class="card-header">
+              <h1>Free and Secure</h1>
+            </div>
+            <div class="card-body fs-5">
+              <ul>
+                <li>
+                  AeroLogger is a completely free service, with the source code
+                  available on <a
+                    href="https://github.com/Oded2/Aerologger/tree/main"
+                    class="text-reset">Github</a
+                  >.
+                </li>
+                <li>
+                  Enjoy a user-friendly experience with no intrusive
+                  advertisements.
+                </li>
+                <li>Make your flight logs public or private to the public.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-4 mb-5">
+          <div class="card h-100">
+            <div class="card-header">
+              <h1>Notices</h1>
+            </div>
+            <div class="card-body fs-5">
+              <ul>
+                <li>
+                  AeroLogger may or may not stay open source in the future.
+                </li>
+                <li>
+                  AeroLogger is still a demo project, nothing is 100% complete.
+                </li>
+                <li>
+                  Please do not share sensitive information with AeroLogger.
+                </li>
+                <li>
+                  While the public may not see any users logs (unless deemed
+                  public by the user), trusted admins have access to each log,
+                  regardless of its public status.
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
