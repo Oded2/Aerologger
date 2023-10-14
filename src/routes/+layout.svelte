000e08b574855d3ca399ba0721668db9311747dd
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-10-15 00:45:36 +0300

    Added icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -38,32 +38,6 @@
   }
 </script>
 
-<!-- <script>
-  import "../global.css";
-  import hrefs from "../data/hrefs.json";
-  import { page } from "$app/stores";
-  import { delelteAllParams } from "../hooks.client.js";
-  export let data;
-  const userId = data.session;
-  let title = "AeroLogger";
-  $: pageUrl = new URL($page.url);
-  $: delelteAllParams(pageUrl);
-  $: pageHref = pageUrl.href.replace(pageUrl.origin, "");
-  $: if (pageHref) {
-    title = findTitle();
-  }
-  function findTitle() {
-    for (let key in hrefs) {
-      const current = hrefs[key];
-      for (let secondKey in current) {
-        const secondCurrent = current[secondKey];
-        if (secondCurrent["link"] == pageHref) {
-          return secondCurrent["title"];
-        }
-      }
-    }
-  }
-</script> -->
 <nav class="navbar navbar-expand-md font-google-quicksand">
   <div class="container">
     <a href={hrefs.home.home.link} class="navbar-brand fw-600">AeroLogger</a>
@@ -84,28 +58,35 @@
           <a
             href={hrefs.home.home.link}
             class="nav-link"
-            class:active={pageHref == hrefs.home.home.link}>Home</a
+            class:active={pageHref == hrefs.home.home.link}
+          >
+            <i class="fa-solid fa-house" /> Home</a
           >
         </li>
         <li class="nav-item">
           <a
             href={hrefs.logbook.home.link}
             class="nav-link"
-            class:active={pageHref == hrefs.logbook.home.link}>Logbook</a
+            class:active={pageHref == hrefs.logbook.home.link}
+          >
+            <i class="fa-solid fa-book" /> Logbook</a
           >
         </li>
         <li class="nav-item">
           <a
             href={hrefs.newFlight.home.link}
             class="nav-link"
-            class:active={pageHref == hrefs.newFlight.home.link}>New Flight</a
+            class:active={pageHref == hrefs.newFlight.home.link}
+          >
+            <i class="fa-solid fa-plane" /> New Log</a
           >
         </li>
         <li class="nav-item">
           <a
             href={hrefs.contact.home.link}
             class="nav-link"
-            class:active={pageHref == hrefs.contact.home.link}>Contact</a
+            class:active={pageHref == hrefs.contact.home.link}
+            ><i class="fa-solid fa-address-card" /> Contact</a
           >
         </li>
       </ul>
