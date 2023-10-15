fea8f4dd350f89fbf1d528af3e32be347cea25ae
Oded2 <obenamotz@gmail.com>
2023-10-16 02:24:48 +0300

    In progress adding rls

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,27 +1,29 @@
 // src/hooks.server.js
-import { redirect, error } from "@sveltejs/kit";
+import { PUBLIC_SUPABASE, PUBLIC_SBURL } from "$env/static/public";
+import { createSupabaseServerClient } from "@supabase/auth-helpers-sveltekit";
 
 export const handle = async ({ event, resolve }) => {
-  // protect requests to all routes that start with /protected-routes
-  if (event.url.pathname.startsWith("/protected-routes")) {
-    const session = await event.locals.getSession();
-    if (!session) {
-      // the user is not signed in
-      throw redirect(303, "/");
-    }
-  }
+  event.locals.supabase = createSupabaseServerClient({
+    supabaseUrl: PUBLIC_SBURL,
+    supabaseKey: PUBLIC_SUPABASE,
+    event,
+  });
 
-  // protect POST requests to all routes that start with /protected-posts
-  if (
-    event.url.pathname.startsWith("/protected") &&
-    event.request.method === "POST"
-  ) {
-    const session = await event.locals.getSession();
-    if (!session) {
-      // the user is not signed in
-      throw error(303, "/");
-    }
-  }
+  /**
+   * a little helper that is written for convenience so that instead
+   * of calling `const { data: { session } } = await supabase.auth.getSession()`
+   * you just call this `await getSession()`
+   */
+  event.locals.getSession = async () => {
+    const {
+      data: { session },
+    } = await event.locals.supabase.auth.getSession();
+    return session;
+  };
 
-  return resolve(event);
+  return resolve(event, {
+    filterSerializedResponseHeaders(name) {
+      return name === "content-range";
+    },
+  });
 };
