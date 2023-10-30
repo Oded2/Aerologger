e5cf8d146c6219c41e7d07095c3f0a3b96d1df0f
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-10-30 16:57:00 +0200

    Edited error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,19 +2,37 @@
   import { page } from "$app/stores";
   import MidScreen from "../components/MidScreen.svelte";
   import logo from "../data/images/logo_simplified.png";
+  import hrefs from "../data/hrefs.json";
   const url = $page;
 </script>
 
 <main>
   <div class="container my-5">
     <MidScreen maxWidth={true}>
-      <div class="card shadow">
+      <div class="card shadow-lg">
         <div class="card-body">
-          <h2 class="card-title">Error</h2>
+          <h2 class="card-title">There's been an error!</h2>
           <h3>Status: {url.status}</h3>
-          <h3>Message: {url.error.message}</h3>
+          <h3>Message:</h3>
+          <h4>{url.error.message}</h4>
         </div>
+        <div class="card-img-bottom d-flex justify-content-center">
+          <a href={hrefs.home.home.link}>
+            <img src={logo} alt="AeroLogger Logo" class="img-fluid" /></a
+          >
+        </div>
+        <div class="card-footer">AeroLogger - Log Your Flights</div>
       </div>
     </MidScreen>
   </div>
 </main>
+
+<svelte:head>
+  <title>Error - AeroLogger</title>
+</svelte:head>
+
+<style>
+  img {
+    max-height: 400px;
+  }
+</style>
