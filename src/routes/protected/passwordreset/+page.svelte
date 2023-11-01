a64e128036aa8448a97bba3b2762a18d4e0ceb3d
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-11-01 15:35:05 +0200

    Updated pass word reset ui

--- src/routes/protected/passwordreset/+page.svelte
+++ src/routes/protected/passwordreset/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import { goto } from "$app/navigation";
+  import MidScreen from "../../../components/MidScreen.svelte";
   import ToastSetup from "../../../components/setup/ToastSetup.svelte";
   import hrefs from "../../../data/hrefs.json";
   import { createToast } from "../../../hooks.client.js";
@@ -50,51 +51,59 @@
 <main>
   <div class="container py-5 font-google-quicksand fs-4">
     {#if session}
-      <form on:submit|preventDefault={submit}>
-        <div class="card">
-          <div class="card-header fs-6">
-            <span>Account: {session.user.email}</span>
-          </div>
-          <div class="card-body fs-3">
-            <div class="mb-3">
-              <label class="fw-bold" for="password">New Password</label>
-              <input
-                type="password"
-                id="password"
-                class="form-control"
-                minlength="8"
-                required
-                bind:value={password}
-              />
-              <div class="form-text">Minimum 8 characters.</div>
+      <MidScreen maxWidth={true}>
+        <form on:submit|preventDefault={submit}>
+          <div class="card">
+            <div class="card-header fs-6">
+              <span>Account: {session.user.email}</span>
             </div>
-            <div class="mb-3">
-              <label class="fw-bold" for="confirmpass"
-                >Confirm New Password {#if password == confirmPass}
-                  <i class="fa-solid fa-circle-check" style="color: #198754;" />
-                {:else}
-                  <i class="fa-solid fa-circle-xmark" style="color: #dc3545;" />
-                {/if}</label
+            <div class="card-body fs-3">
+              <div class="mb-3">
+                <label class="fw-bold" for="password">New Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  class="form-control"
+                  minlength="8"
+                  required
+                  bind:value={password}
+                />
+                <div class="form-text">Minimum 8 characters.</div>
+              </div>
+              <div class="mb-3">
+                <label class="fw-bold" for="confirmpass"
+                  >Confirm New Password {#if password == confirmPass}
+                    <i
+                      class="fa-solid fa-circle-check"
+                      style="color: #198754;"
+                    />
+                  {:else}
+                    <i
+                      class="fa-solid fa-circle-xmark"
+                      style="color: #dc3545;"
+                    />
+                  {/if}</label
+                >
+                <input
+                  type="password"
+                  id="confirmpass"
+                  class="form-control"
+                  minlength="8"
+                  required
+                  bind:value={confirmPass}
+                />
+              </div>
+            </div>
+            <div class="card-footer">
+              <button
+                class="btn btn-primary fs-4 fw-bold w-100"
+                type="submit"
+                disabled={!isValid || inProgress}>Reset Password</button
               >
-              <input
-                type="password"
-                id="confirmpass"
-                class="form-control"
-                minlength="8"
-                required
-                bind:value={confirmPass}
-              />
             </div>
           </div>
-          <div class="card-footer">
-            <button
-              class="btn btn-primary fs-4 fw-bold w-100"
-              type="submit"
-              disabled={!isValid || inProgress}>Reset Password</button
-            >
-          </div>
-        </div>
-      </form>
+        </form></MidScreen
+      >
     {:else}
       <h1>You are not signed in.</h1>
     {/if}
