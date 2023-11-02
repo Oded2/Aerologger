3e2d85d84de99bc4cc198b89e4e98cc938b55065
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-11-02 13:47:50 +0200

    Updated error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,37 +1,24 @@
 <script>
   import { page } from "$app/stores";
-  import MidScreen from "../components/MidScreen.svelte";
-  import logo from "../data/images/logo_simplified.png";
   import hrefs from "../data/hrefs.json";
   const url = $page;
 </script>
 
 <main>
-  <div class="container my-5">
-    <MidScreen maxWidth={true}>
-      <div class="card shadow-lg">
-        <div class="card-body">
-          <h2 class="card-title">There's been an error!</h2>
-          <h3>{url.status}</h3>
-          <h4>{url.error.message}</h4>
-        </div>
-        <div class="card-img-bottom d-flex justify-content-center">
-          <a href={hrefs.home.home.link}>
-            <img src={logo} alt="AeroLogger Logo" class="img-fluid" /></a
-          >
-        </div>
-        <div class="card-footer">AeroLogger - Log Your Flights</div>
-      </div>
-    </MidScreen>
+  <div class="container py-5">
+    <div class="my-5 text-center font-google-quicksand">
+      <h1 class="fw-600">{`${url.status} Error`}</h1>
+      <h2 class="fw-600">{url.error.message}</h2>
+      <h6>
+        If this was unexpected, please <a
+          href={hrefs.contact.home.link}
+          class="text-reset">contact me</a
+        >
+      </h6>
+    </div>
   </div>
 </main>
 
 <svelte:head>
   <title>{url.status} Error</title>
 </svelte:head>
-
-<style>
-  img {
-    max-height: 400px;
-  }
-</style>
