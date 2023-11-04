a4750edd74de1c00a0ef49925f790a72b799d13d
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-11-04 14:42:13 +0200

    Added better error messages

--- src/routes/protected/newflight/+page.js
+++ src/routes/protected/newflight/+page.js
@@ -8,8 +8,12 @@ export async function load({ url, parent }) {
   if (logId) {
     const { data } = await supabase.from("Logs").select().eq("id", logId);
     log = data[0];
-    if (!log || (log.user_id !== session.user.id && edit == false))
+    if (!log)
       throw error(401, { message: `Log ${logId} does not belong to you.` });
+    if (edit && log.user_id !== session.user.id)
+      throw error(401, {
+        message: `Log ${logId} can only be edited by its owner.`,
+      });
   }
   return { logId, log, edit };
 }
