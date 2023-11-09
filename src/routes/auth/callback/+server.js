2d439efc8a0b15512ab3778b546b6db6fa99083d
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-11-09 14:24:00 +0200

    Removed line break

--- src/routes/auth/callback/+server.js
+++ src/routes/auth/callback/+server.js
@@ -2,10 +2,8 @@ import { redirect } from "@sveltejs/kit";
 
 export const GET = async ({ url, locals: { supabase } }) => {
   const code = url.searchParams.get("code");
-
   if (code) {
     await supabase.auth.exchangeCodeForSession(code);
   }
-
   throw redirect(303, "/");
 };
