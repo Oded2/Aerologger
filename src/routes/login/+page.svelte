3220689eb509c607fa036311550788fd3aa88df4
FIRST_NAME LAST_NAME <oded@benamotz.com>
2023-12-16 23:50:57 +0200

    Removed comment

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -111,57 +111,6 @@
         />
       </div>
     </Card>
-    <!-- <div class="card">
-        <div class="card-header">
-          <span
-            >Don't have an account? <a
-              href={oldHrefs.signup.home.link}
-              class="text-reset">Sign Up</a
-            ></span
-          >
-        </div>
-        <div class="card-body fs-3">
-          <div class="mb-3">
-            <label for="email" class="form-label"
-              >Email <span title="Required field" class="required">*</span
-              ></label
-            >
-            <input
-              type="email"
-              id="email"
-              class="form-control"
-              bind:value={email}
-              required
-            />
-          </div>
-
-          <div>
-            <label for="password" class="form-label"
-              >Password <span title="Required field" class="required">*</span
-              ></label
-            >
-            <input
-              type="password"
-              id="password"
-              class="form-control"
-              bind:value={password}
-              required
-            />
-            <button
-              class="btn btn-error btn-outline mt-3"
-              type="reset"
-              onclick="forgotpass.showModal()">Forgot Password?</button
-            >
-          </div>
-        </div>
-        <div class="card-footer px-sm-5">
-          <button
-            class="btn btn-primary btn-lg fs-4 fw-bold w-100"
-            disabled={inProgress}
-            type="submit">Login</button
-          >
-        </div>
-      </div> -->
   </Container>
 </main>
 <ToastSetup {toast} />
