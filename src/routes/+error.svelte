2a26cf8e6637abd7ffc55ee79f6140358605571e
Oded <obenamotz@gmail.com>
2023-12-22 23:15:01 +0200

    Fixed error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,16 +1,17 @@
 <script>
   import { page } from "$app/stores";
+  import Container from "$lib/components/Container.svelte";
   import Title from "$lib/components/Title.svelte";
   const url = $page;
 </script>
 
 <main>
-  <div class="container py-5">
-    <div class="my-5 text-center font-google-quicksand">
-      <h1 class="fw-600">{`${url.status} Error`}</h1>
-      <h2 class="fw-600">{url.error.message}</h2>
+  <Container>
+    <div class="font-semibold text-center">
+      <h1 class="text-4xl">{`${url.status} Error`}</h1>
+      <h2 class="text-3xl">{url.error.message}</h2>
     </div>
-  </div>
+  </Container>
 </main>
 
 <Title title={`${url.status} Error`}></Title>
